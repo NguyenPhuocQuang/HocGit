309329ea13efbdf99120ad07ff646280d83fb4e8
Nguyen Phuoc Quang <50256278+NguyenPhuocQuang@users.noreply.github.com>
2022-02-13 09:28:19 +0700

    Update main.py
    add two numbers

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pip import main
 def print_string(str):
     print(str)
 
+def cal_numbers(nub_1, nub2):
+    return nub_1+nub2
 if __name__ == "__main__":
     print_string("hello world")
     
