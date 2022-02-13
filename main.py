704e932200795bb808d6b509bf99ed6c1e803484
QuangNP1 <npquang14041999@gmail.com>
2022-02-13 09:30:30 +0700

    add sub_number

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pip import main
 def print_string(str):
     print(str)
 
+def sub_number(nub1, nub2):
+    return nub1-nub2
+    
 if __name__ == "__main__":
     print_string("hello world")
-    
